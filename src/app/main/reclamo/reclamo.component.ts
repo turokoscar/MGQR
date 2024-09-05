92cecf769e5db0c409ed331a11adb4d00b5a2c3e
Oscar Javier Pazos Mogollón <turokoscar@gmail.com>
2024-09-05 15:01:55 -0500

    Formularios reactivos
    Habilito opcion de reactividad

--- src/app/main/reclamo/reclamo.component.ts
+++ src/app/main/reclamo/reclamo.component.ts
@@ -24,6 +24,8 @@ export class ReclamoComponent implements OnInit {
   ngOnInit(): void {
     this.showPrimerForm();
     this.showSegundoForm();
+    // Observa cambios en la variable disabled
+    this.onToggleConfidentiality();
   }
 
   //4. Estructuro la primera parte del formulario
@@ -37,29 +39,52 @@ export class ReclamoComponent implements OnInit {
   //5. Estructuro la segunda parte del formulario
   private showSegundoForm():void{
     this.segundaParteForm = this.fb.group({
-      tipo_documento: ['', Validators.required],
-      numero_documento: ['', [Validators.required]],
+      tipo_documento: [{ value: '', disabled: false }, Validators.required],
+      numero_documento: [{ value: '', disabled: false }, Validators.required],
       genero: ['', Validators.required],
-      nombre: ['', [Validators.required, Validators.maxLength(100)]],
-      apellido_paterno: ['', [Validators.required, Validators.maxLength(100)]],
-      apellido_materno: ['', [Validators.required, Validators.maxLength(100)]],
+      nombre: [{ value: '', disabled: false }, Validators.required],
+      apellido_paterno: [{ value: '', disabled: false }, Validators.required],
+      apellido_materno: [{ value: '', disabled: false }, Validators.required],
       departamento: ['', Validators.required],
       provincia: ['', Validators.required],
       distrito: ['', Validators.required],
-      direccion: ['', [Validators.required, Validators.maxLength(255)]],
+      direccion: ['', Validators.required],
       numero_telefono: [''],
       numero_celular: [''],
-      correo_electronico: ['', [Validators.required, Validators.email]],
-      comunidad: ['', [Validators.required, Validators.maxLength(255)]],
+      correo_electronico: ['', Validators.required],
+      comunidad: ['', Validators.required],
       cargo: ['', Validators.required],
       tipo_consulta: ['', Validators.required],
-      contenido_consulta: ['', [Validators.required, Validators.maxLength(500)]],
+      contenido_consulta: ['', Validators.required],
       evidencia_consulta: ['']
-    })
+    });
   }
 
   onTipoPersonaChange(event: any) {
     this.showEmailField = event.value === '1';
+  }
+
+  onToggleConfidentiality(): void {
+    // Deshabilita o habilita los campos según el valor de disabled
+    if (this.disabled) {
+      this.segundaParteForm.controls['tipo_documento'].disable();
+      this.segundaParteForm.controls['numero_documento'].disable();
+      this.segundaParteForm.controls['nombre'].disable();
+      this.segundaParteForm.controls['apellido_paterno'].disable();
+      this.segundaParteForm.controls['apellido_materno'].disable();
+    } else {
+      this.segundaParteForm.controls['tipo_documento'].enable();
+      this.segundaParteForm.controls['numero_documento'].enable();
+      this.segundaParteForm.controls['nombre'].enable();
+      this.segundaParteForm.controls['apellido_paterno'].enable();
+      this.segundaParteForm.controls['apellido_materno'].enable();
+    }
+  }
+
+  // Método que se ejecuta cuando cambia el checkbox de confidencialidad
+  activaConfidencialidad(): void {
+    this.disabled = !this.disabled; // Cambia el estado de confidencialidad
+    this.onToggleConfidentiality();  // Aplica la lógica de activación/desactivación
   }
 
   onSubmit():void{
