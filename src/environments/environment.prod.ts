8deb65237efcba6b6e7e73333e49d7d325e05728
rblas0527 <rblas0527@gmail.com>
2024-09-07 05:44:58 -0500

    feat:Configuracion de ambientes uat y prod

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    apiUrl: 'http://localhost:5000',
+    apiUbigeo: 'https://test.inia.gob.pe/api-general'
+  };
+  
---
+++ src/environments/environment.uat.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: false,
+    apiUrl: 'http://localhost:4000',
+    apiUbigeo: 'https://test.inia.gob.pe/api-general'
+  };
+  
