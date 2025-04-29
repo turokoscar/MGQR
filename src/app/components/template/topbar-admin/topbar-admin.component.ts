049121d304ebdcf2b690d8646d6d4319240eac69
joseval1 <josevalenzuelasalvador@gmail.com>
2025-04-29 03:33:22 -0500

    feat: Cambios de diseño login, sidebar, topbar, footer

--- src/app/components/template/topbar-admin/topbar-admin.component.ts
+++ src/app/components/template/topbar-admin/topbar-admin.component.ts
@@ -8,7 +8,15 @@ import { Router } from '@angular/router';
 })
 export class TopbarAdminComponent {
   constructor(private router: Router) { }
-
+  menuUsuarioActivo: boolean = false;
+  usuario = {
+    nombre: 'Pepito Ramos Chumpitaz',
+    dni: '12345678',
+    correo: 'pepito@example.com'
+  };
+  toggleUserMenu() {
+    this.menuUsuarioActivo = !this.menuUsuarioActivo;
+  }
   logout():void{
     //Aca puedo añadir procesos para borrar tokens y demas que deben definirse
 
